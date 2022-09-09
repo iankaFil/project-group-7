58fa001a0e6e4378ff6f0e6cf4685a1496ae591a
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-09-09 18:31:39 +0000

    Deploying to gh-pages from @ iankaFil/project-group-7@0eb63e53bd1d6fbe343136933f8c4f7c1ec90379 🚀

--- contact.6694387d.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t;e=document.querySelector("[contact-menu-button]"),t=document.querySelector("[contact-menu]"),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),document.body.classList.toggle("menu-open")}))}();
-//# sourceMappingURL=contact.6694387d.js.map
---
+++ contact.f40b638a.js
@@ -0,0 +1,2 @@
+(()=>{const t=document.querySelector("[contact-menu-button]"),e=document.querySelector("[contact-menu]");t.addEventListener("click",(()=>{const c="true"===t.getAttribute("aria-expanded")||!1;t.classList.toggle("is-open"),t.setAttribute("aria-expanded",!c),e.classList.toggle("is-open"),document.body.classList.toggle("contact-open")}))})();
+//# sourceMappingURL=contact.f40b638a.js.map
