391acf1dc743beafc32d85470381cdaabe61ed22
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-09-09 19:57:26 +0000

    Deploying to gh-pages from @ iankaFil/project-group-7@793a172e178c774d8de470c6140969776bd9ca5b 🚀

--- contact.d45c6863.js
+++
@@ -1,2 +0,0 @@
-!function(){var t,e;t=document.querySelector("[contact-menu-button]"),e=document.querySelector("[contact-menu]"),t.addEventListener("click",(function(){var n="true"===t.getAttribute("aria-expanded")||!1;t.classList.toggle("is-open"),t.setAttribute("aria-expanded",!n),e.classList.toggle("is-open"),document.body.classList.toggle("contact-open")}))}();
-//# sourceMappingURL=contact.d45c6863.js.map
