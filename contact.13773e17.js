e616227675de451afb669dc829b647bddc3960c2
iankaFil <108305015+iankaFil@users.noreply.github.com>
2022-09-09 16:46:17 +0000

    Deploying to gh-pages from @ iankaFil/project-group-7@1ed18126e013225c4cbb770b7baac306670ad4fd 🚀

---
+++ contact.13773e17.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[contact-menu-button]"),t=document.querySelector("[contact-menu]");e.addEventListener("click",(()=>{const n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),document.body.classList.toggle("menu-open")}))})();
+//# sourceMappingURL=contact.13773e17.js.map
